26f71a59683e9f924464cad5f2bb0764f941e5da
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-25 21:51:31 +0000

    21 Choose Rounds (range(Rounds Buttons))
    for item in range(0, 3):
          self.rounds_button = Button(self.how_many_frame, fg=button_fg, bg=btn_color_value[item][0], text="{} Rounds".format(btn_color_value[item][1]), font=button_font_small, width=10, command=lambda i=item: self.to_play(btn_color_value[i][1]))
          self.rounds_button.grid(row=0, column=item, padx=5, pady=5)

--- main.py
+++ main.py
@@ -21,15 +21,43 @@ class ChooseRounds:
     #Rounds Buttons...
     self.how_many_frame = Frame(self.intro_frame)
     self.how_many_frame.grid(row=2)
-    
-    self.three_button = Button(self.how_many_frame, fg=button_fg, bg="#CC0000", text="3 Rounds", font=button_font_small, width=10)
-    self.three_button.grid(row=0, column=0, padx=5, pady=5)
-    
-    self.five_button = Button(self.how_many_frame, fg=button_fg, bg="#009900", text="5 Rounds", font=button_font_small, width=10)
-    self.five_button.grid(row=0, column=1, padx=5, pady=5)
+    # list set up rounds button. first coulour, second number of rounds.
+    btn_color_value = [["#CC0000", 3],["#009900", 5],["#000099", 10]]
 
-    self.ten_button = Button(self.how_many_frame, fg=button_fg, bg="#009900", text="10 Rounds", font=button_font_small, width=10)
-    self.ten_button.grid(row=0, column=2, padx=5, pady=5)
+    for item in range(0, 3):
+      self.rounds_button = Button(self.how_many_frame, fg=button_fg, bg=btn_color_value[item][0], text="{} Rounds".format(btn_color_value[item][1]), font=button_font_small, width=10, command=lambda i=item: self.to_play(btn_color_value[i][1]))
+      self.rounds_button.grid(row=0, column=item, padx=5, pady=5)
+
+  def to_play(self, num_rounds):
+    Play(num_rounds)
+    
+    # hide intro window
+    root.withdraw()
+
+class Play:
+  def __init__(self, how_many):
+    self.play_box = Toplevel()
+    # if users press cross at top, closes help and 'releases' help button
+    self.play_box.protocol('WM_DELETE_WINDOW', partial(self.close_play))
+    self.quest_frame = Frame(self.play_box, padx=10, pady=10)
+    self.quest_frame.grid()
+
+    rounds_heading = "Choose - Round 1 of {}".format(how_many)
+    self.choose_heading = Label(self.quest_frame, text=rounds_heading, font=("Arial", "16", "bold"))
+    self.choose_heading.grid(row=0)
+
+    self.control_frame = Frame(self.quest_frame)
+    self.control_frame.grid(row=6)
+    
+    self.start_over_button = Button(self.control_frame, text="Start Over", command=self.close_play)
+    self.start_over_button.grid(row=0, column=2)
+  
+  def close_play(self):
+    # reshow
+    root.deiconify()
+    self.play_box.destroy()
+    
+    
     
 
 if __name__ == "__main__":
